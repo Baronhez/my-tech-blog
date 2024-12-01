49b2d6f43c9a6d5c4e30b6bcc7dc5655bdd21862
Jonathan Ródenas López <57847247+Baronhez@users.noreply.github.com>
2024-12-01 20:37:41 +0100

    Change domain and base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://ideas.jonthan.xyz/",
+  base: "idea-manager",
   trailingSlash: "always",
   integrations: [
     tailwind(),
